3fae154bee82efad72ccdb1b6b2efdd6a698dddb
Eric Pietrowicz <pietrowicz.eric@gmail.com>
2019-05-22 09:50:23 -0500

    Update SD_write.ino

--- SD_write.ino
+++ SD_write.ino
@@ -3,51 +3,86 @@
 #include <SD.h>
 #include <SD_t3.h>
 
-/*
-  SD card read/write
- 
- This example shows how to read and write data to and from an SD card file   
- The circuit:
- * SD card attached to SPI bus as follows:
- ** MOSI - pin 11, pin 7 on Teensy with audio board
- ** MISO - pin 12
- ** CLK - pin 13, pin 14 on Teensy with audio board
- ** CS - pin 4, pin 10 on Teensy with audio board
- 
- created   Nov 2010
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- 
- This example code is in the public domain.
-   
- */
+#include <Wire.h>
+#include <TimerOne.h>
 
+#define    MPU9250_ADDRESS            0x68
+#define    MAG_ADDRESS                0x0C
+
+#define    GYRO_FULL_SCALE_250_DPS    0x00
+#define    GYRO_FULL_SCALE_500_DPS    0x08
+#define    GYRO_FULL_SCALE_1000_DPS   0x10
+#define    GYRO_FULL_SCALE_2000_DPS   0x18
+
+#define    ACC_FULL_SCALE_2_G        0x00
+#define    ACC_FULL_SCALE_4_G        0x08
+#define    ACC_FULL_SCALE_8_G        0x10
+#define    ACC_FULL_SCALE_16_G       0x18
 
 File myFile;
 
-// change this to match your SD shield or module;
-// Arduino Ethernet shield: pin 4
-// Adafruit SD shields and modules: pin 10
-// Sparkfun SD shield: pin 8
-// Teensy audio board: pin 10
-// Teensy 3.5 & 3.6 on-board: BUILTIN_SDCARD
-// Wiz820+SD board: pin 4
-// Teensy 2.0: pin 0
-// Teensy++ 2.0: pin 20
 const int chipSelect = 10;
+int incomingByte = 0;
+
+// Initial time
+long int ti;
+volatile bool intFlag = false;
+
+// This function read Nbytes bytes from I2C device at address Address.
+// Put read bytes starting at register Register in the Data array.
+void I2Cread(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data)
+{
+  // Set register address
+  Wire.beginTransmission(Address);
+  Wire.write(Register);
+  Wire.endTransmission();
+
+  // Read Nbytes
+  Wire.requestFrom(Address, Nbytes);
+  uint8_t index = 0;
+  while (Wire.available())
+    Data[index++] = Wire.read();
+}
+
+// Write a byte (Data) in device (Address) at register (Register)
+void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
+{
+  // Set register address
+  Wire.beginTransmission(Address);
+  Wire.write(Register);
+  Wire.write(Data);
+  Wire.endTransmission();
+}
+
 
 void setup()
 {
- //UNCOMMENT THESE TWO LINES FOR TEENSY AUDIO BOARD:
-//  SPI.setMOSI(11);  // Audio shield has MOSI on pin 7
-//  SPI.setSCK(13);  // Audio shield has SCK on pin 14
-  
- // Open serial communications and wait for port to open:
+
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-   while (!Serial) {
+  while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
+  Wire.begin();
+
+  // Set accelerometers low pass filter at 5Hz
+  I2CwriteByte(MPU9250_ADDRESS, 29, 0x06);
+  // Set gyroscope low pass filter at 5Hz
+  I2CwriteByte(MPU9250_ADDRESS, 26, 0x06);
+
+  // Configure gyroscope range
+  I2CwriteByte(MPU9250_ADDRESS, 27, GYRO_FULL_SCALE_1000_DPS);
+  // Configure accelerometers range
+  I2CwriteByte(MPU9250_ADDRESS, 28, ACC_FULL_SCALE_4_G);
+  // Set by pass mode for the magnetometers
+  I2CwriteByte(MPU9250_ADDRESS, 0x37, 0x02);
+
+  // Request continuous magnetometer measurements in 16 bits
+  I2CwriteByte(MAG_ADDRESS, 0x0A, 0x16);
+
+  // Store initial time
+  ti = millis();
+
 
 
   Serial.print("Initializing SD card...");
@@ -57,41 +92,132 @@ void setup()
     return;
   }
   Serial.println("initialization done.");
-  
+
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
   myFile = SD.open("test.txt", FILE_WRITE);
-  
+
   // if the file opened okay, write to it:
   if (myFile) {
     Serial.print("Writing to test.txt...");
     myFile.println("testing 1, 2, 3.");
-  // close the file:
-    myFile.close();
-    Serial.println("done.");
   } else {
     // if the file didn't open, print an error:
     Serial.println("error opening test.txt");
   }
-  
-  // re-open the file for reading:
-  myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial.println("test.txt:");
-    
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
+
+
+
+
+
+
+
 }
 
 void loop()
 {
+
+  // ____________________________________
+  // :::  accelerometer and gyroscope :::
+
+  // Read accelerometer and gyroscope
+  uint8_t Buf[14];
+  I2Cread(MPU9250_ADDRESS, 0x3B, 14, Buf);
+
+  // Create 16 bits values from 8 bits data
+
+  // Accelerometer
+  int16_t ax = -(Buf[0] << 8 | Buf[1]);
+  int16_t ay = -(Buf[2] << 8 | Buf[3]);
+  int16_t az = Buf[4] << 8 | Buf[5];
+
+  // Gyroscope
+  int16_t gx = -(Buf[8] << 8 | Buf[9]);
+  int16_t gy = -(Buf[10] << 8 | Buf[11]);
+  int16_t gz = Buf[12] << 8 | Buf[13];
+
+  myFile.print(millis() - ti, DEC);
+  myFile.print("\t");
+
+
+  // Accelerometer
+  myFile.print (ax, DEC);
+  myFile.print ("\t");
+  myFile.print (ay, DEC);
+  myFile.print ("\t");
+  myFile.print (az, DEC);
+  myFile.print ("\t");
+
+  // Gyroscope
+  myFile.print (gx, DEC);
+  myFile.print ("\t");
+  myFile.print (gy, DEC);
+  myFile.print ("\t");
+  myFile.print (gz, DEC);
+  myFile.print ("\t");
+
+
+
+  // _____________________
+  // :::  Magnetometer :::
+
+
+  // Read register Status 1 and wait for the DRDY: Data Ready
+
+  uint8_t ST1;
+  do
+  {
+    I2Cread(MAG_ADDRESS, 0x02, 1, &ST1);
+  }
+  while (!(ST1 & 0x01));
+
+  // Read magnetometer data
+  uint8_t Mag[7];
+  I2Cread(MAG_ADDRESS, 0x03, 7, Mag);
+
+
+  // Create 16 bits values from 8 bits data
+
+  // Magnetometer
+  int16_t mx = -(Mag[3] << 8 | Mag[2]);
+  int16_t my = -(Mag[1] << 8 | Mag[0]);
+  int16_t mz = -(Mag[5] << 8 | Mag[4]);
+
+
+  // Magnetometer
+  myFile.print (mx + 200, DEC);
+  myFile.print ("\t");
+  myFile.print (my - 70, DEC);
+  myFile.print ("\t");
+  myFile.print (mz - 700, DEC);
+  myFile.print ("\t");
+
+
+
+  // End of line
+  myFile.println("");
+
+  incomingByte = Serial.read();
   // nothing happens after setup
+  if (incomingByte == 1) {
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+    // re-open the file for reading:
+    myFile = SD.open("test.txt");
+    if (myFile) {
+      Serial.println("test.txt:");
+
+      // read from the file until there's nothing else in it:
+      while (myFile.available()) {
+        Serial.write(myFile.read());
+      }
+      // close the file:
+      myFile.close();
+    } else {
+      // if the file didn't open, print an error:
+      Serial.println("error opening test.txt");
+    }
+  }
+
 }
